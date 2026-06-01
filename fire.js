008058cfe4616539b1993529a72b6ddc6eaca09e
gabrielac24 <maiden242004@gmail.com>
2026-05-31 20:36:19 -0600

    Seguridad Firebase con variables de entorno

--- fire.js
+++ fire.js
@@ -1,8 +1,12 @@
 const admin = require('firebase-admin');
 
-// ¡OJO AQUÍ! Cambia 'nombre-de-tu-archivo.json' por el nombre EXACTO 
-// del archivo de credenciales que descargaste de Firebase.
-const serviceAccount = require('./clavearva.json');
+// 🔒 Le decimos al código que busque las llaves en el "aire" (Variables de Entorno)
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  // Esta línea mágica arregla los saltos de línea de la llave privada
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
